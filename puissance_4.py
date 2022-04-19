4b493cbb3c322cdabf72a69a10079fdb0ee5debc
elie kanga <elie.kanga@ens.uvsq.fr>
2022-04-19 11:02:10 +0200

    squellete du programme

---
+++ puissance_4.py
@@ -0,0 +1,51 @@
+#########################################
+# groupe BI TD 1
+# Elie KANGA
+# Sarah Medeneche
+# Iram MADANI FOUATIH
+# https://github.com/uvsq22101259/puisssance_4
+#########################################
+
+
+#########################################
+# importation librairie                 #
+#########################################
+
+import tkinter as tk
+
+
+#########################################
+# definitions des constantes            #
+#########################################
+
+HEIGHT = 400
+WIDTH = 400
+
+
+#########################################
+#  definitions des variables            #
+#########################################
+
+
+
+
+#########################################
+# definitions des fonctions             #
+#########################################
+
+
+
+
+
+
+
+
+#########################################
+# programme principal                   #
+#########################################
+
+
+racine = tk.Tk()
+racine.title("TAS DE SABLE")
+canvas = tk.Canvas(racine,width= WIDTH, height= HEIGHT, bg= "blue")
+canvas.grid(column=3,row=0, rowspan= 20)
