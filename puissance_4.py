d4da550b1cb04de080e82d565bb788000751755b
elie kanga <elie.kanga@ens.uvsq.fr>
2022-04-19 12:04:57 +0200

    coloriage des cases

--- puissance_4.py
+++ puissance_4.py
@@ -12,20 +12,21 @@
 #########################################
 
 import tkinter as tk
+from unittest import case
 
 
 #########################################
 # definitions des constantes            #
 #########################################
 
-HEIGHT = 400
-WIDTH = 400
+TAILLE = 600
+CASE = 6
 
 
 #########################################
 #  definitions des variables            #
 #########################################
-
+case_id = []
 
 
 
@@ -33,7 +34,36 @@ WIDTH = 400
 # definitions des fonctions             #
 #########################################
 
+def grillage(n,taille):
+    """ cree une grille de n^2 case, et donne a chaque case une couleur selectionner en fonction du grain de sable qu'elle contient """
+    global case_id
+    
+    if len(case_id) > 0:
+        for i in case_id:
+            canvas.delete(i)
+        case_id = []
+    
+    rythme = taille // n
+    x = 0
+    y = 3
+    
+    for i in range (n):
+        x = 3
+        for j in range(n):
+            case_id.append(canvas.create_oval((x,y),(x+rythme,y+rythme), fill="grey" ,outline="blue", width= 2 ))
+            x += rythme
+        y += rythme
 
+def mode_player(event):
+    """permet a l'utilisateur de donner des grains de sable lui-meme"""
+    j = canvas.find_closest( event.x, event.y)
+    # print(j[0])
+    canvas.itemconfig(j[0], fill ="yellow")
+
+    
+        
+        
+    
 
 
 
@@ -47,5 +77,8 @@ WIDTH = 400
 
 racine = tk.Tk()
 racine.title("TAS DE SABLE")
-canvas = tk.Canvas(racine,width= WIDTH, height= HEIGHT, bg= "blue")
-canvas.grid(column=3,row=0, rowspan= 20)
+canvas = tk.Canvas(racine,width= TAILLE, height= TAILLE, bg= "blue")
+canvas.grid(column=0,row=0, rowspan= 20)
+canvas.bind("<Button-1>",mode_player)
+grillage(CASE,TAILLE)
+racine.mainloop()
